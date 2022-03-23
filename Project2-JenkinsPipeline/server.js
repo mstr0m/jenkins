f4504889614797a3d7449f143e027424f786475c
Mike <mikestromm@gmail.com>
2022-03-23 10:35:33 +0000

    change to webapp

--- Project2-JenkinsPipeline/server.js
+++ Project2-JenkinsPipeline/server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World! Mike is here :)');
+  res.send('Hello World! Mike is here :) And an update to web app');
 });
 
 app.listen(PORT, HOST);
